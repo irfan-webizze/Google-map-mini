1a42aaa7e5eca5c39f86b32b4d68a1b7fed697e3
irfan-webizze <irfan@webizze.com>
2024-11-14 17:53:23 +0530

    updated markers on main file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomMap } from "./Maps";
 
 const user = new User();
 console.log(user);
 
 const company = new Company();
 console.log(company);
+
+const map = new CustomMap("map");
+map.addMarker(user);
+map.addMarker(company);
+console.log(map);
