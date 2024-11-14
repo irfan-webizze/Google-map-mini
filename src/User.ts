89fd1a9a7e204c42c2fe70d2e30d8e6b04ebfe19
irfan-webizze <irfan@webizze.com>
2024-11-14 17:51:28 +0530

    generated user details

--- src/User.ts
+++ src/User.ts
@@ -1,16 +1,20 @@
 import { faker } from "@faker-js/faker";
-
-export class User {
+import { Mappable } from "./Maps";
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = "red";
   constructor() {
     (this.name = faker.name.firstName()),
       (this.location = {
         lat: parseFloat(faker.address.latitude()),
         lng: parseFloat(faker.address.longitude()),
       });
+  }
+  contentString(): string {
+    return `<h3>USER: ${this.name}</h3>`;
   }
 }
